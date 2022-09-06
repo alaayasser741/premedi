9126f0186667e46c69c6f111b1383d4855521964
Alaa Yasser <74079336+alaayasser17@users.noreply.github.com>
2022-09-06 17:55:37 +0200

    add Navbar

--- script/main.js
+++ script/main.js
@@ -2,21 +2,21 @@ let navbarToggle = document.querySelector(".toggle i");
 let closeSideMenu = document.querySelector(".close i");
 let sideMenu = document.querySelector(".links");
 let overlay = document.querySelector(".overlay");
+let searchForm = document.querySelector(".search-form");
+let searchBtn = document.querySelector(".search i");
+
 
 if (navbarToggle) {
   navbarToggle.addEventListener("click", () => {
     sideMenu.style.left = "0";
     overlay.style.display = "block";
-    myNav.classList.add("nav-colored");
-    myNav.classList.remove("nav-transparent");
   });
 }
 if (closeSideMenu) {
   closeSideMenu.addEventListener("click", () => {
     sideMenu.style.left = "-105%";
     overlay.style.display = "none";
-    myNav.classList.add("nav-transparent");
-    myNav.classList.remove("nav-colored");
+
   });
 }
 if (overlay) {
@@ -25,15 +25,7 @@ if (overlay) {
     overlay.style.display = "none";
   });
 }
-// nav Scroll
-let myNav = document.querySelector("#nav");
-console.log(myNav)
-window.onscroll = function () {
-  if (document.body.scrollTop >= 97|| document.documentElement.scrollTop > 97) {
-    myNav.classList.add("nav-colored");
-    myNav.classList.remove("nav-transparent");
-  } else {
-    myNav.classList.add("nav-transparent");
-    myNav.classList.remove("nav-colored");
-  }
-};
+
+  searchBtn.addEventListener("click", () => {
+    searchForm.classList.toggle('formPos');
+  });
